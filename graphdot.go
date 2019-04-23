3d8aa7587f45b8b8f5ed8a2b166090940aa0fc86
Eric Wohltman <eric.wohltman@gmail.com>
2019-04-22 23:54:14 -0400

    Build dependency map and output DOT format

---
+++ graphdot.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type (
+	dependencyMap map[string][]string
+	packageHashes map[string]uint32
+)
+
+func mapDependencies(dependencies []string) (packageHashes, dependencyMap) {
+	hashes := make(packageHashes)
+	dependsUpon := make(dependencyMap)
+
+	for _, dependency := range dependencies {
+		if dependency == "" {
+			continue
+		}
+
+		relationship := strings.Split(dependency, " ")
+
+		if _, found := hashes[relationship[0]]; !found {
+			hashes[relationship[0]] = packageHash(relationship[0])
+		}
+
+		if _, found := hashes[relationship[1]]; !found {
+			hashes[relationship[1]] = packageHash(relationship[1])
+		}
+
+		dependsUpon[relationship[0]] = append(
+			dependsUpon[relationship[0]],
+			relationship[1],
+		)
+	}
+
+	return hashes, dependsUpon
+}
+
+func packageHash(dependency string) uint32 {
+	hash := fnv.New32a()
+
+	_, err := hash.Write([]byte(dependency))
+	if err != nil {
+		log.SetOutput(os.Stderr)
+		log.Fatalf("Error hashing package: %s\n", err)
+	}
+
+	return hash.Sum32()
+}
+
+func dotFormat(hashes packageHashes, dependsUpon dependencyMap) *bytes.Buffer {
+	buf := bytes.NewBuffer([]byte{})
+
+	buf.WriteString("digraph {\n")
+	buf.WriteString("    node [shape=box];\n")
+
+	for libraryDetails, hash := range hashes {
+		library := strings.Split(libraryDetails, "@")
+		libraryName := ""
+		libraryVersion := ""
+
+		if len(library) < 2 {
+			libraryName = library[0]
+		} else {
+			libraryName = library[0]
+			libraryVersion = library[1]
+		}
+
+		nodeLabel := ""
+
+		if libraryVersion != "" {
+			nodeLabel = fmt.Sprintf(
+				"    %v [label=\"%s\\n%s\"];\n",
+				hash,
+				libraryName,
+				libraryVersion,
+			)
+		} else {
+			nodeLabel = fmt.Sprintf(
+				"    %v [label=\"%s\"];\n",
+				hash,
+				libraryName,
+			)
+		}
+
+		buf.WriteString(nodeLabel)
+	}
+
+	for child, parents := range dependsUpon {
+		for _, parent := range parents {
+			buf.WriteString(
+				fmt.Sprintf(
+					"    %v -> %v;\n",
+					hashes[child],
+					hashes[parent],
+				),
+			)
+		}
+	}
+
+	buf.WriteString("}")
+
+	return buf
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stdout)
+
+	_, err := os.Stat("go.mod")
+	if err != nil {
+		log.SetOutput(os.Stderr)
+		log.Fatalf("Error finding go.mod file: %s\n", err)
+	}
+
+	cmdOutput, err := exec.
+		Command("go", "mod", "graph").
+		Output()
+	if err != nil {
+		log.SetOutput(os.Stderr)
+		log.Fatalf("Error running go mod graph: %s\n", err)
+	}
+
+	log.Println(
+		dotFormat(
+			mapDependencies(
+				strings.Split(
+					string(cmdOutput), "\n",
+				),
+			),
+		).String(),
+	)
+}
